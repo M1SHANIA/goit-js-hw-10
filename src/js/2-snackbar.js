3b03d7969952e6237c3465d482cfeff6a51404b1
Mykhailo Onipchenko <95632067+M1SHANIA@users.noreply.github.com>
2024-02-13 21:42:39 +0100

    isDone

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,34 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+document.querySelector(".form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const delay = parseInt(this.elements.delay.value);
+    const state = this.elements.state.value;
+
+    const snackbarPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === "fulfilled") {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+
+    snackbarPromise
+        .then((delay) => {
+            iziToast.success({
+                title: "Fulfilled promise",
+                message: `✅ Fulfilled promise in ${delay}ms`,
+            });
+        })
+        .catch((delay) => {
+            iziToast.error({
+                title: "Rejected promise",
+                message: `❌ Rejected promise in ${delay}ms`,
+            });
+        });
+    event.target.reset();
+});
